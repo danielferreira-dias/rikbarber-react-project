92b56f0f70bdd9b6747d3d3099d1e28ae39e0802
Daniel Dias <danieldias.it98@gmail.com>
2024-06-27 12:00:58 +0100

    First Commit of Files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <section className='w-full bg-black h-svh'></section>
+    </div>
+  )
+}
+
+export default App
