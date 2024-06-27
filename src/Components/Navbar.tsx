db5f5dce7b709e598c4b6b325d23c3277385ce83
Daniel Dias <danieldias.it98@gmail.com>
2024-06-27 17:38:09 +0100

    Fix bug

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -10,28 +10,29 @@ const Navbar = () => {
     };
 
     const menuItems = [
-        { id: 1, label: 'Menu 1' },
-        { id: 2, label: 'Menu 2' },
-        { id: 3, label: 'Menu 3' },
-        { id: 4, label: 'Menu 4' },
-        { id: 5, label: 'Menu 5' },
+        { id: 1, label: 'Home' },
+        { id: 2, label: 'About us' },
+        { id: 3, label: 'Services' },
+        { id: 4, label: 'Booking' },
+        { id: 5, label: 'Shop' },
     ];
 
     return (
         <>
-            <div className="h-14 lg:h-20 xl:h-28 2xl:h-32 bg-zinc-950 sticky lg:relative top-0 z-50">
-                <div className='px-10 lg:px-20 xl:px-32 2xl:px-52 flex justify-between items-center h-full'>
+            <div className="h-14 xs:h-20 lg:h-20 xl:h-28 2xl:h-32 bg-zinc-950 sticky lg:relative top-0 z-50 text-lg lg:text-xl xl:text-2xl">
+                <div className='px-6 lg:px-20 xl:px-30 2xl:px-52 flex justify-between items-center h-full'>
                     <div className='h-14 w-32 xl:w-72'>
-                        <img src="https://cutstyle.true-emotions.studio/dark-beard/wp-content/uploads/sites/4/2018/08/logo-db2.svg" alt="Logo" className='h-14 w-24 lg:h-18 lg:w-32 xl:w-72' />
+                        <img src="https://cutstyle.true-emotions.studio/dark-beard/wp-content/uploads/sites/4/2018/08/logo-db2.svg" alt="Logo" className='h-14 w-24 md:w-28 lg:h-18 lg:w-32 xl:w-72' />
                     </div>
-                    <div className='hidden lg:flex '>
+                    <div className='hidden md:flex gap-x-2'>
                         {menuItems.map(item => (
-                            <div key={item.id} className="py-5 text-white font-semibold text-xl xl:text-2xl ">
+                            <div key={item.id} className="py-5 text-white font-semibold hover:transform hover:scale-105 transition-transform">
                                 <a href="" className='p-5'>{item.label}</a>
                             </div>
                         ))}
+                        <a href="" className='text-white font-semibold p-5 hover:transform hover:scale-105 transition-transform'>Contact</a>
                     </div>
-                    <div className='lg:hidden'>
+                    <div className='md:hidden'>
                         <svg onClick={toggleMenu} className="h-10 w-fit cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" stroke="#eee" strokeWidth=".6" fill="rgba(0,0,0,0)" strokeLinecap="round">
                             <path d="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7">
                                 <animate dur="0.2s" attributeName="d" values="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7;M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7" fill="freeze" begin="start.begin" />
@@ -48,15 +49,18 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-            <div className={`fixed top-14 right-0 h-svh bg-neutral-800 text-white font-semibold shadow-lg p-4 z-40 w-2/3 xs:w-80 transition ease-in-out delay-150 duration-300 overflow-scroll ${menuVisible ? 'translate-x-0' : 'translate-x-full'}`}>
-                <ul>
+            <div className={`fixed text-lg lg:text-xl xl:text-2xl md:hidden top-14 xs:top-20 lg:top-20 xl:top-28 2xl:top-32 right-0 h-screen bg-neutral-800 text-white font-semibold shadow-lg p-2 py-0 z-40 w-2/3 xs:w-80 transition ease-in-out delay-150 duration-300 overflow-auto ${menuVisible ? 'translate-x-0' : 'translate-x-full'}`}>
+                <ul className='py-3'>
                     {menuItems.map(item => (
-                        <li key={item.id} className="py-5 border-b-2 border-neutral-900 border-opacity-30">
-                            <a href="" className='p-5'>{item.label}</a>
+                        <li key={item.id} className="py-2 border-b-2 border-neutral-900 border-opacity-30 hover:transform hover:scale-105 transition-transform">
+                            <a href="#" className="p-4 block">{item.label}</a>
                         </li>
                     ))}
-                    <li className="py-5">
-                        <a href="" className='p-5'>Menu 6</a>
+                    <li className="py-2 hover:transform hover:scale-105 transition-transform">
+                        <a href="#" className="p-4 block">Contact</a>
+                    </li>
+                    <li className="py-2 hover:transform hover:scale-105 transition-transform">
+                        <a href="#" className="p-4 block"></a>
                     </li>
                 </ul>
             </div>
