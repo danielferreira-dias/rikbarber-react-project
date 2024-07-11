6ea24ff0ecb6007f0b83ef132d05dddd05d804b6
Daniel Dias <daniel.dias@ludustechnology.com>
2024-07-11 17:43:15 +0100

    fix for iOS

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -55,8 +55,8 @@ const Navbar = () => {
                             </Link>
                         ))}
                     </div>
-                    <div className='md:hidden h-12 w-fit relative'>
-                        <svg onClick={toggleMenu} className="h-12 w-fit cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" stroke="#eee" strokeWidth=".6" fill="rgba(0,0,0,0)" strokeLinecap="round">
+                    <div className='md:hidden h-12 w-12 relative'>
+                        <svg onClick={toggleMenu} className="h-12 w-12 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" stroke="#eee" strokeWidth=".6" fill="rgba(0,0,0,0)" strokeLinecap="round">
                             <path d="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7">
                                 <animate dur="0.2s" attributeName="d" values="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7;M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7" fill="freeze" begin="start.begin" />
                                 <animate dur="0.2s" attributeName="d" values="M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7;M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7" fill="freeze" begin="reverse.begin" />
